dafebaae88ef9ced51420bdc71967379893aa887
Sebastian Correa <sebas.11992@hotmail.com>
2019-09-22 10:36:57 -0500

    Ejemplo de calculadora

---
+++ Ejemplo1POO/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo1POO
+{
+    public partial class Form1 : Form
+    {
+
+        Matematicas matematicas = new  Matematicas();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsumar_Click(object sender, EventArgs e)
+        {
+            matematicas.setNumero1(double.Parse(txtnumero1.Text));
+            matematicas.setNumero2(double.Parse(txtnumero2.Text));
+            lblresultado.Text = "El resultado es: " + matematicas.sumar().ToString();
+            lblresultado.Visible = true;
+
+        }
+
+        private void btnrestar_Click(object sender, EventArgs e)
+        {
+            matematicas.setNumero1(double.Parse(txtnumero1.Text));
+            matematicas.setNumero2(double.Parse(txtnumero2.Text));
+            lblresultado.Text = "El resultado es: " + matematicas.restar().ToString();
+            lblresultado.Visible = true;
+        }
+
+        private void btnmultiplicar_Click(object sender, EventArgs e)
+        {
+            matematicas.setNumero1(double.Parse(txtnumero1.Text));
+            matematicas.setNumero2(double.Parse(txtnumero2.Text));
+            lblresultado.Text = "El resultado es: "+matematicas.multiplicar().ToString();
+            lblresultado.Visible = true;
+        }
+
+        private void btndividir_Click(object sender, EventArgs e)
+        {
+            matematicas.setNumero1(double.Parse(txtnumero1.Text));
+            matematicas.setNumero2(double.Parse(txtnumero2.Text));
+            lblresultado.Text = "El resultado es: "+matematicas.dividir().ToString();
+            lblresultado.Visible = true;
+        }
+    }
+}
---
+++ Ejemplo1POO/Matematicas.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo1POO
+{
+    public class Matematicas
+    {
+        //se definen los atributos o veriables de la clase,
+        //por recomendacion se crean de manera privada.
+
+        private double numero1;
+        private double numero2;
+
+        public void setNumero1(double valor)
+        {
+            this.numero1 = valor;
+        }
+
+        public double getNumero1()
+        {
+            return this.numero1;
+        }
+
+        public void setNumero2(double valor)
+        {
+            this.numero2 = valor;
+        }
+
+        public double getNumero2()
+        {
+            return this.numero2;
+        }
+
+        public double sumar()
+        {
+            return (this.getNumero1() + this.getNumero2());
+        }
+
+        public double restar()
+        {
+            return (this.getNumero1() - this.getNumero2());
+        }
+
+        public double multiplicar()
+        {
+            return (this.getNumero1() * this.getNumero2());
+        }
+
+        public double dividir()
+        {
+            return (this.getNumero1() / this.getNumero2());
+        }
+    }
+}
